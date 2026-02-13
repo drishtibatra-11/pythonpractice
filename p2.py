ef35f31e68179ad158b3cb4c6fa7712cbd2318dc
drishti batra <batradrishti22@gmail.com>
2026-02-13 22:24:36 +0530

    p2.py

---
+++ p2.py
@@ -0,0 +1,10 @@
+empty_set =  set()
+print(empty_set)
+    numbers = {1, 2, 3, 4}
+print(numbers)
+
+mixed_set= {1, 3.14, "python", (1, 2,3,4)}
+colors_list = ["red", "green", "blue"]
+colors_set = set(colors_list)
+print(colors_list)
+print(colors_set)
