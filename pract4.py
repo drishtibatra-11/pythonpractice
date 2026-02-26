7f885d7af8762f4e887981b3a6bfb4ecb0ca2fd9
drishti batra <batradrishti22@gmail.com>
2026-02-26 21:05:24 +0530

    pract4.py

---
+++ pract4.py
@@ -0,0 +1,6 @@
+#count and display the number of capital letters in a string
+string = input("Enter a string: ")
+for ch in string:
+    if ch.upper()==ch
+        count+=1
+print("number of capital letters: ",count)
