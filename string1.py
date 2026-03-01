8ef0dc03e2f02d25b43a55bc78cd671058075bb0
drishti batra <batradrishti22@gmail.com>
2026-03-01 14:24:34 +0530

    p50.py

---
+++ string1.py
@@ -0,0 +1,8 @@
+#count and display the capital letters in sentence
+str="My name is Drishti Batra"
+count=0
+for ch in str:
+    if  ch.upper() ==ch:
+        count+=1         
+        
+print(count)
---
+++ string2.py
@@ -0,0 +1,3 @@
+str=input ("enter a string: ")
+words=str.split()
+print("the words in the string are: ", words)
