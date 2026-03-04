8cc835a78582c865cd7e1c9e1eefe99e0e3609a3
drishti batra <batradrishti22@gmail.com>
2026-03-04 12:07:41 +0530

    function8.py

--- function8.py
+++ function8.py
@@ -1,4 +1,4 @@
-#Write a program to check whether all the values in a dictionary are same or not using lambda function
+#Write a program to check whether all the values in a dictionary are same or not using lambda function.
 my_dict = {"a": 100, "b": 100, "c": 100}
 
 check_same = lambda d: len(set(d.values())) == 1
