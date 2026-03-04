f7ac2e775f53ac417830fb8b5d3c63ef196b5b87
drishti batra <batradrishti22@gmail.com>
2026-03-04 12:09:00 +0530

    function2.py

--- function2.py
+++ function2.py
@@ -1,4 +1,4 @@
-#.     Write a Python function that takes a positive integer and returns the sum of the cube of all the positive integers smaller than the specified number
+#.     Write a Python function that takes a positive integer and returns the sum of the cube of all the positive .integers smaller than the specified number
 def sum_of_cubes(n):
     total = 0
     for i in range(1, n):
