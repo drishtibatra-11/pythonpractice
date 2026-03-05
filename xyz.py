06914377b1d976754e04728c51a5647cfbe0a7fe
drishti batra <batradrishti22@gmail.com>
2026-03-05 13:26:00 +0530

    xyz.py

---
+++ xyz.py
@@ -0,0 +1,10 @@
+string = input("Enter the string: ")
+substring = input("Enter the substring: ")
+
+count = 0
+
+for i in range(len(string) - len(substring) + 1):
+    if string[i:i+len(substring)] == substring:
+        count += 1
+
+print(count)
