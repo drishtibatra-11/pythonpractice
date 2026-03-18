6cdeac6c59b7612e419430184434ade8e9c81529
drishti batra <batradrishti22@gmail.com>
2026-03-18 10:16:20 +0530

    p6.py

--- p6.py
+++ p6.py
@@ -1,4 +1,4 @@
-#swap to get common employee  code for 3 departement
+#swap to get common employee  code for 3 departement.
 dep1={"100","101","102","103","104","105"p7. 
 ,"106","107","108","109"}
 dep2={"100","121","103","106","108"}
