bdaa25f5d3e5cd5328a39028f4c28e277375a2f5
drishti batra <batradrishti22@gmail.com>
2026-04-20 21:29:38 +0530

    partition.py

---
+++ train.py
@@ -0,0 +1,54 @@
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+train_folder = "C:/Users/Asus/Desktop/crop_project/train"
+test_folder  = "C:/Users/Asus/Desktop/crop_project/test"
+
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    rotation_range=20,
+    zoom_range=0.2,
+    horizontal_flip=True
+)
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+train_data = train_datagen.flow_from_directory(
+    train_folder,
+    target_size=(224,224),
+    batch_size=32,
+    class_mode='binary'
+)
+
+test_data = test_datagen.flow_from_directory(
+    test_folder,
+    target_size=(224,224),
+    batch_size=32,
+    class_mode='binary'
+)
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Conv2D(32, (3,3), activation='relu', input_shape=(224,224,3)),
+    tf.keras.layers.MaxPooling2D(2,2),
+
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+
+    tf.keras.layers.Conv2D(128, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+
+model.compile(
+    optimizer='adam',
+    loss='binary_crossentropy',
+    metrics=['accuracy']
+)
+
+model.fit(train_data, epochs=10, validation_data=test_data)
+
+model.save("crop_model.h5")
+print("Model saved successfully")
