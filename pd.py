7898c44a6c66568bcb20639fb106490d9a6a6bde
drishti batra <batradrishti22@gmail.com>
2026-04-24 10:17:51 +0530

    pd.py

---
+++ pd.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+s = pd.Series([10,20,30,40,50,68,70,80,90,100])
+print(s)
+print("Head",s.head())
+print("Tail",s.tail())
+print("add 5 \n",s+5)
+print("mul 2 \n",s*2)
