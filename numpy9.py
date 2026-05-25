1da61e3fdf485c74a5469e2da7677f622e2dfc86
drishti batra <batradrishti22@gmail.com>
2026-05-25 23:34:45 +0530

    numpy.9.py

--- numpy9.py
+++ numpy9.py
@@ -1,6 +1,7 @@
+// This code demonstrates various NumPy functions for array operations such as sum, mean, standard deviation, square root, power, and exponential.          
 import numpy as np
 
-arr = np.array[(12,7,45,3,29)]
+arr = np.array([12,7,45,3,29])
 print("sum",np.sum(arr))
 print("mean",np.mean(arr))
 print("std dev",np.std(arr))
